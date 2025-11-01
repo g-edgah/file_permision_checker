a14b49a95183ba6ccace4ce9019f6904a2e598fa
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-11-01 11:20:01 +0300

    functions

--- permision_checker.py
+++ permision_checker.py
@@ -18,16 +18,17 @@ def check_writable(directory="."):
     
     print(f"scanning for writable files in {directory}")
 
-    for file in directory.rglob('*'):
+    for file_path in directory.rglob('*'):
         try:
-            if file.is_file():
-                file_stat = file.stat()
+            if file_path.is_file():
+                file_stat = file_path.stat()
                 permissions = file_stat.st_mode
 
                 if permissions & stat.S_IWOTH:
-                    writable.append(file)
+                    writable.append(file_path)
+                
         except (PermissionError, OSError) as e:
-            print(f"cannot access {file} - {e}")
+            print(f"cannot access {file_path} - {e}")
 
     return writable
 
@@ -39,20 +40,23 @@ def results(writable):
     print(f"{len(writable)} world writable files found")
     print("")
 
-    for i, file in enumerate(writable, 1):
+    for i, file_path in enumerate(writable, 1):
         try:
-            file_stat = file.stat()
-            permissions = oct(file.st_mode)[1-3:]
-            size = file.st_size
-            owner = file.owner
+            file_stat = file_path.stat()
+            permissions = oct(file_stat.st_mode)[-3:]
+            size = file_stat.st_size
+            owner = file_path.owner()
 
-            print(f"    Permissions: {permissions} \nSize: {size:>8}bytes \nOwner: {owner}")
+            print(f"{i:2d}. {file_path}")
+            print(f"    Permissions: {permissions} \n    Size: {size:>8}bytes \n    Owner: {owner}")
 
         except OSError as e:
-            print(f"{i:2d}. {file},- Error: {e}")
+            print(f"{i:2d}. {file_path},- Error: {e}")
 
 def main():
     directory = sys.argv[1] if len(sys.argv) > 1 else "."
 
     writable = check_writable(directory)
     results(writable)
+
+main()
